3eba13fd31ac2e9b22b299789ff5cfe9c2758c1e
DOVENE AGOGUE <adovene@gmail.com>
2021-10-11 09:36:28 +0200

    add swagger to generate api documentation

---
+++ src/main/java/com/dev/apistarter/config/SwaggerUiWebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.dev.apistarter.config;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+public class SwaggerUiWebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
