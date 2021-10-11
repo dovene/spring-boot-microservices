87a7216845f83f036f229d8946c997adc8593b26
DOVENE AGOGUE <adovene@gmail.com>
2021-10-11 10:53:44 +0200

    add actuator to monitor api

--- src/main/java/com/dev/apistarter/ApiStarterApplication.java
+++ src/main/java/com/dev/apistarter/ApiStarterApplication.java
@@ -1,7 +1,10 @@
 package com.dev.apistarter;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
@@ -10,6 +13,11 @@ public class ApiStarterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiStarterApplication.class, args);
+	}
+
+	@Bean
+	public HttpTraceRepository htttpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
 	}
 
 }
